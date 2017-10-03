c3d880e87b24086e83c77db860bac81ee79ef850
innoccent <867913275@qq.com>
2017-10-03 23:45:49 +0800

    index

--- js/index.js
+++ js/index.js
@@ -1,92 +1,146 @@
-$(document).ready(function () {
-    //添加固定头部颜色
-    var scrollTop;
-    $(window).scroll(function () {
-        scrollTop=$(document).scrollTop();
-        if(scrollTop>0){
-            $('.top').css('display','block');
-            $('nav .nav-inner').addClass('active');
-        }else{
-            $('.top').css('display','none');
-            $('nav .nav-inner').removeClass('active');
-        }
+/*
+* @Author: lenovo
+* @Date:   2017-07-20 21:35:22
+* @Last Modified by:   lenovo
+* @Last Modified time: 2017-07-21 10:16:23
+*/
 
-        //bussiness下移
-        if(scrollTop>$('.bussiness-center').offset().top&&scrollTop<$('.bussiness-center').offset().top+$('.bussiness-center').height()){
-            $('.bussiness-inner').css('transform','translateY('+-($('.bussiness-center').offset().top-scrollTop)+'px)')
-        }
+'use strict';
+$(function(){
+	$(".home>a").click(function(){
+		$(".down").slideToggle(500)
+	});
 
+	var top=$(".bgm").find(".page");
+	var tops=top.offset().top;
+	var cy=$(document.body).scrollTop();
+	var flag=true;
+	var me=$("#me").offset().top-500;
+	var one=$(".one");
+	var cw=window.innerWidth;
+	window.onresize=function(){
+		console.log(window.innerWidth)
+		cw=window.innerWidth;
+			if(cw<996){
+				flag=false;
+				$(".banner").css("display","none")
+			}
+	}
+	window.onscroll=function(){
+		cy=$(document.body).scrollTop();
+		var scr=0;
+		one.each(function(index,value){
+			if(cy>=$(value).offset().top-350){
+				$(".banner-l li").removeClass("border").eq(index).addClass('border');
+			}
+		})
+		if(tops<cy){
+			if(flag&&cw>=996){
+				flag=false;
+				$(".banner").slideDown('300', function() {
+					flag=true;
+				});	
+			};
+		}else{
+			flag=false;
+			$(".banner").slideUp("300",function(){
+					flag=true;
+			})
+		}
+		if(me<cy){
+			$("#me").find(".auto").addClass("animated rotateIn")
+		}
 
-        //设置移动端下移
-        if(scrollTop>=$('.mobel').height()*2&&scrollTop<=$('.mobel').height()*3){
-            $('.mobel-center').css('transform','translateY('+-($('.mobel').height()*2-scrollTop)+'px)');
-        }
-
-        //设置种类帧动画
-        if(scrollTop>=$('.kindcase2').scrollTop()+$('.kindcase2').height()){
-            $('.kindcase2.active>a').addClass('active')
-        }
-        //设置种类下移
-        if(scrollTop>=$('.kind-center').scrollTop()+$('.kind-center').height()&&scrollTop<=$('.kind-center').scrollTop()+$('.kind-center').height()*2){
-            $('.kind-center').css('transform','translateY('+-($('.kind-center').scrollTop()+$('.kind-center').height()-scrollTop)+'px)')
-        }
-        //banner图下移
-        if(scrollTop>$(window).height()){
-            return;
-        }
-        $('.center-img').css('transform','translateY('+scrollTop+'px)')
-
-    })
-
-    $('.top').click(function () {
-        $(document.body).animate({scrollTop:0},1000,function () {
-            console.log($(document.body).scrollTop())
-        })
-    });
-    //设置种类初选项卡
-    $('ul>li').click(function () {
-        $('ul>li').removeClass('active');
-        $(this).addClass('active');
-        var now = $(this).index();
-        $('.kindcase2').removeClass('active').eq(now).addClass('active').children('a').addClass('active')
-    })
-
-    //小屏状态下选项卡
-    //屏幕改变时清除选项卡
-    var rg = $('.kindcase1.active').index()/2;
-    $('.kindcase1').click(function () {
-        $('.kindcase1').removeClass('active');
-        var now = $(this).index()/2;
-        if(rg==now){
-            $(this).removeClass('active');
-            $('.kindcase2').removeClass('active').eq(now).removeClass('active');
-        }else{
-            rg=now;
-            $(this).addClass('active');
-            $('.kindcase2').removeClass('active').eq(now).addClass('active').children('a').addClass('active')
-        }
-    })
-
-    //利用swiper设计轮播图
-    var swiper = new Swiper('.mobel .swiper-container',{
-        centeredSlides: true,
-        paginationClickable: true,
-        grabCursor: true,
-        pagination: '.swiper-pagination',
-        paginationType: 'fraction'
-    })
-
-    //customers
-    var swipers = new Swiper('.customers .swiper-container',{
-        scrollbar: '.swiper-scrollbar',
-        scrollbarHide: true,
-        slidesPerView: 'auto',
-        // centeredSlides: true,
-        // spaceBetween: 30,
-        slidesPerColumn: 2,
-        grabCursor: true
-    })
-
+	}
+	
+	// 轮播图
+ 	var bo=$(".bo");
+ 	var lis=$(".fang>li");
+ 	var cir=$(".circlew li")
+ 	var now=0;
+ 	var next=0;
+ 	var kai=true;
+ 	var width=bo.width();
+ 	function move(map="r"){
+ 		if(!kai){
+ 			return;
+ 		}
+ 		kai=false;
+ 		if(map=="r"){
+ 			next=now+1;
+	 		if(next>=lis.length){
+	 			next=0;
+	 		};
+	 		lis.eq(next).css("left",width);
+ 			lis.eq(now).animate({left:-width},300,'linear');
+ 			lis.eq(next).animate({left:0},300,'linear',function(){
+ 				kai=true;
+ 			});
+ 		}
+ 		if(map =='l'){
+ 			next=now-1;
+	 		if(next<0){
+	 			next=lis.length-1;
+	 		};
+	 		lis.eq(next).css("left",-width);
+ 			lis.eq(now).animate({left:width},300,'linear');
+ 			lis.eq(next).animate({left:0},300,'linear',function(){
+ 				kai=true;
+ 			});
+ 		};
+ 		cir.removeClass().eq(next).addClass('active');
+ 		now=next;
+ 	}
+	var t=setInterval(move,5000);
+	$(".left").click(function(){
+		move("l")
+	})
+	$(".right").click(function(){
+		move("r")
+	})
+	cir.click(function(){
+		var index=$(this).index();
+		if(index>now){
+			if(!kai){
+ 			return;
+	 		}
+	 		kai=false;
+			index=now-1;
+	 		if(index<0){
+	 			index=lis.length-1;
+	 		};
+	 		lis.eq(index).css("left",-width);
+ 			lis.eq(now).animate({left:width},300,'linear');
+ 			lis.eq(index).animate({left:0},300,'linear',function(){
+ 				kai=true;
+ 			});
+		}
+		if(index<now){
+			if(!kai){
+ 			return;
+	 		}
+	 		kai=false;
+ 			index=now+1;
+	 		if(index>=lis.length){
+	 			index=0;
+	 		};
+	 		lis.eq(index).css("left",width);
+ 			lis.eq(now).animate({left:-width},300,'linear');
+ 			lis.eq(index).animate({left:0},300,'linear',function(){
+ 				kai=true;
+ 			});
+		}
+		if(index==now){
+			kai=true;
+		}
+		cir.removeClass().eq(index).addClass('active');
+		now=index;
+	})
+	bo.hover(function(){
+		clearInterval(t);
+	},function(){
+		t=setInterval(move,5000)
+	})
 
 
 
